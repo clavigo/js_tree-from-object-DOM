9efbe4058bed2a6a4df72cf20915d922936ce759
Oleksandr Rad <clavigo365@gmail.com>
2024-11-11 17:11:21 +0200

    make build

---
+++ index.9f8bf630.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var l in t){var a=document.createElement("li");a.textContent=l,"object"==typeof t[l]&&null!==t[l]&&e(a,t[l]),r.appendChild(a)}r.childNodes.length>0&&n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.9f8bf630.js.map
